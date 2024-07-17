b7928207aca353776cd6f8ae2bc499e323fcbd44
Veryyoung <2429148505@qq.com>
2024-07-17 13:18:00 +0800

    modified:   hoist_base/launch/hoist_base.launch.py 	modified:   hoist_base/launch/hoist_mapping.launch.py

--- hoist_base/launch/hoist_base.launch.py
+++ hoist_base/launch/hoist_base.launch.py
@@ -61,15 +61,6 @@ def generate_launch_description():
          output='screen',
          parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
-    start_mapping = launch_ros.actions.Node(
-        package='slam_toolbox',
-        executable='async_slam_toolbox_node',
-        name='slam_toolbox',
-        parameters=[
-            slam_toolbox_mapping_file_dir,
-            {'use_sim_time': LaunchConfiguration('use_sim_time'),}
-        ]
-    )
     lidar_start = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(os.path.join(rplidar,'launch','rplidar_a2m8_launch.py')),
                 launch_arguments= {
@@ -77,12 +68,8 @@ def generate_launch_description():
                 }.items()
                 
             )
-    start_navigation2 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(navigation2_launch_dir, 'hoist_navigation.py')),
-        launch_arguments={
-            'use_sim_time': LaunchConfiguration('use_sim_time'),
-            # 'map': nav2_map_dir,
-            'params_file': nav2_params_file_dir}.items()
+    start_nav_mapping = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_share, 'hoist_mapping.launch.py')),
     )
     ld = LaunchDescription()
     ld.add_action(model)
@@ -91,8 +78,7 @@ def generate_launch_description():
     ld.add_action(robot_state_publisher_node)
     ld.add_action(joint_state_publisher_node)
     ld.add_action(lidar_start)
-    ld.add_action(start_mapping)
     ld.add_action(robot_localization_node)
-    ld.add_action(start_navigation2)
+    ld.add_action(start_nav_mapping)
     ld.add_action(declare_world_cmd)
     return ld
