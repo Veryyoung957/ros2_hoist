4a3f9db66e582a38b8dfa208be80109e40515bae
Veryyoung <2429148505@qq.com>
2024-07-29 20:20:00 +0800

    modified:   hoist_base/urdf/hoist.urdf

---
+++ hoist_base/launch/hoist_localization.launch.py
@@ -0,0 +1,45 @@
+import launch
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+import launch_ros
+import launch_ros.descriptions
+import os
+from launch import LaunchDescription
+from launch.conditions import LaunchConfigurationEquals, LaunchConfigurationNotEquals
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+def generate_launch_description():
+    pkg_share = get_package_share_directory('hoist_base')
+    default_model_path = os.path.join(pkg_share, 'urdf/hoist.urdf')
+    slam_toolbox_localization_file_dir = os.path.join(pkg_share, 'config', 'mapper_params_localization.yaml')
+    navigation2_launch_dir = os.path.join(get_package_share_directory('hoist_navigation'), 'launch')
+    # nav2_params_file_dir = os.path.join(pkg_share, 'config', 'nav2_params_real.yaml')
+    params_file = LaunchConfiguration('params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='False')
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(pkg_share, 'config', 'nav2_params.yaml'),
+        description='Full path to the ROS2 parameters file to use for all launched nodes')
+
+    slam_toolbox_mapping_file_dir = os.path.join(pkg_share, 'config', 'mapper_params_online_async.yaml')
+    start_mapping = launch_ros.actions.Node(
+        parameters=[
+            slam_toolbox_mapping_file_dir,
+            {'use_sim_time': True}
+        ],
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen'
+    )
+    start_navigation2 = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(navigation2_launch_dir, 'navigation_launch.py')),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'params_file': params_file}.items()
+    )
+    ld = LaunchDescription()
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(start_mapping)
+    ld.add_action(start_navigation2)
+    return ld
